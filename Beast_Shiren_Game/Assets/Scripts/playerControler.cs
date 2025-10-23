b4c8d56f94d2f5292649573f81572f68e0504dd9
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-10-23 16:25:49 +0100

    Added sidescroller controles, need to redone it i think

--- Beast_Shiren_Game/Assets/Scripts/playerControler.cs
+++ Beast_Shiren_Game/Assets/Scripts/playerControler.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
-
+using System.Collections;
+using UnityEngine.Animations;
 public class playerControler : MonoBehaviour
 {
+    //Movement Variables
+    public float runSpeed;
+    Rigidbody myRB;
+    Animator myAnim;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody>();
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        myAnim.SetFloat("speed", Mathf.Abs(move));
+
+        myRB.linearVelocity = new Vector3(move * runSpeed, myRB.linearVelocity.y, 0);
     }
 }
